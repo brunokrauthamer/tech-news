de4c047616a6d61e4e7f68fa25a4b666d9dafcd2
Bruno Krauthamer <brunokrauthamer@gmail.com>
2023-03-15 16:05:24 -0300

    req04

--- tech_news/scraper.py
+++ tech_news/scraper.py
@@ -1,6 +1,7 @@
 import requests
 import time
 from parsel import Selector
+from bs4 import BeautifulSoup
 
 
 # Requisito 1
@@ -30,9 +31,45 @@ def scrape_next_page_link(html_content):
     return link
 
 
+def remove_white_spaces(string):
+    new_string = string
+    while new_string[-1] == ' ':
+        new_string = new_string[0:-1]
+    return new_string
+
+
+def scrape_summary(html):
+    selector = Selector(text=html)
+    parag = selector.css(".entry-content").get()
+    soup = BeautifulSoup(parag, "html.parser")
+    return soup.p.get_text()
+
+
 # Requisito 4
 def scrape_news(html_content):
-    """Seu código deve vir aqui"""
+    selector = Selector(text=html_content)
+    url = selector.css("[rel=\"canonical\"]::attr(href)").get()
+    title = selector.css(".entry-title::text").get()
+    timestamp = selector.css(".meta-date::text").get()
+    writer = selector.css(".author a::text").get()
+    reading_time_text = selector.css(".meta-reading-time::text").get()
+    summary = scrape_summary(html_content)
+    category = selector.css(".label::text").get()
+    reading_time = int(reading_time_text[0:-19])
+    treated_title = title.replace('\xa0', '')
+    treated_title = remove_white_spaces(treated_title)
+    treated_summary = summary.replace('\xa0', '')
+    treated_summary = remove_white_spaces(treated_summary)
+
+    return {
+        "url": url,
+        "title": treated_title,
+        "timestamp": timestamp,
+        "writer": writer,
+        "reading_time": reading_time,
+        "summary": treated_summary,
+        "category": category
+    }
 
 
 # Requisito 5
