abdf266043330f68dd6faeab6e111b937ba46050
Bruno Krauthamer <brunokrauthamer@gmail.com>
2023-03-16 19:46:53 -0300

    reqs 08 and 09

--- tech_news/analyzer/search_engine.py
+++ tech_news/analyzer/search_engine.py
@@ -1,4 +1,5 @@
 from tech_news.database import find_news
+from datetime import datetime
 
 
 # Requisito 7
@@ -11,11 +12,30 @@ def search_by_title(title):
     return filtered_news
 
 
+def validate_date_format(date):
+    try:
+        date_object = datetime.strptime(date, "%Y-%m-%d")
+    except ValueError:
+        raise ValueError('Data inválida')
+    return date_object.strftime('%d/%m/%Y')
+
+
 # Requisito 8
 def search_by_date(date):
-    """Seu código deve vir aqui"""
+    br_date = validate_date_format(date)
+    news = find_news()
+    filtered_news = []
+    for new in news:
+        if new['timestamp'] == br_date:
+            filtered_news.append((new['title'], new['url']))
+    return filtered_news
 
 
 # Requisito 9
 def search_by_category(category):
-    """Seu código deve vir aqui"""
+    news = find_news()
+    filtered_news = []
+    for new in news:
+        if category.lower() == new['category'].lower():
+            filtered_news.append((new['title'], new['url']))
+    return filtered_news
